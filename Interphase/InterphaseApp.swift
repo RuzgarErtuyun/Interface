cc3900f28c6993d4fd251128cea1eae21993ecfb
Ruzgar <ruzgar.ertuyun@hisarschool.k12.tr>
2024-12-18 14:19:49 +0300

    Initial Commit

---
+++ Interphase/InterphaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterphaseApp.swift
+//  Interphase
+//
+//  Created by Ruzgar Ertuyun on 18.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct InterphaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InterphaseTests/InterphaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  InterphaseTests.swift
+//  InterphaseTests
+//
+//  Created by Ruzgar Ertuyun on 18.12.2024.
+//
+
+import Testing
+@testable import Interphase
+
+struct InterphaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
